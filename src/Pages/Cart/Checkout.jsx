66526f84591a34d6a8292ce8c974f77c8fc6867f
sasikiran0 <sasikran@bloomitsolution.in>
2024-05-31 18:01:25 +0530

    fix

---
+++ src/Pages/Cart/Checkout.jsx
@@ -0,0 +1,300 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getCartItems } from "../../Redux/cartSlice";
+import { getAllProducts } from "../../Redux/productSlice";
+
+function Checkout() {
+  const dispatch = useDispatch();
+  const { productList } = useSelector((state) => state.product);
+  const { cart } = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    dispatch(getCartItems());
+    dispatch(getAllProducts());
+  }, [dispatch]);
+
+  const cartItems = cart
+    .map((cartItem) => {
+      const product = productList.find((product) => product.id === cartItem.id);
+      return product ? { ...cartItem, ...product } : null;
+    })
+    .filter((item) => item !== null);
+
+  return (
+    <div>
+      <div className="flex flex-col items-center border-b bg-white py-4 sm:flex-row sm:px-10 lg:px-20 xl:px-32">
+        <a href=" #" className="text-2xl font-bold text-gray-800">
+          Mana Storez
+        </a>
+      </div>
+
+      <div className="grid sm:px-10 lg:grid-cols-2 lg:px-20 xl:px-32">
+        <div className="px-4 pt-8">
+          <p className="text-xl font-medium">Order Summary</p>
+          <p className="text-gray-400">
+            Check your items. And select a suitable shipping method.
+          </p>
+          <div className="mt-8 space-y-3  border bg-white px-2 py-4 sm:px-6">
+            {cartItems.map((item) => (
+              <div
+                key={item.id}
+                className="flex flex-col  bg-white sm:flex-row"
+              >
+                <img
+                  className="m-2 h-24 w-28  border object-cover object-center"
+                  src={item.image}
+                  alt={item.name}
+                />
+                <div className="flex w-full flex-col px-4 py-4">
+                  <span className="font-semibold">{item.name}</span>
+                  <span className="float-right text-gray-400">{item.size}</span>
+                  <p className="text-lg font-bold">₹{item.price}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <p className="mt-8 text-lg font-medium">Shipping Methods</p>
+          <form className="mt-5 grid gap-6">
+            <div className="relative">
+              <input
+                className="peer hidden"
+                id="radio_1"
+                type="radio"
+                name="radio"
+                defaultChecked
+              />
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full  border-8 border-gray-300 bg-white"></span>
+              <label
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none border border-gray-300 p-4"
+                htmlFor="radio_1"
+              >
+                <img
+                  className="w-14 object-contain"
+                  src="/images/naorrAeygcJzX0SyNI4Y0.png"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">Fedex Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">
+                    Delivery: 2-4 Days
+                  </p>
+                </div>
+              </label>
+            </div>
+            <div className="relative">
+              <input
+                className="peer hidden"
+                id="radio_2"
+                type="radio"
+                name="radio"
+              />
+              <span className="peer-checked:border-gray-700 absolute right-4 top-1/2 box-content block h-3 w-3 -translate-y-1/2 rounded-full border-8 border-gray-300 bg-white"></span>
+              <label
+                className="peer-checked:border-2 peer-checked:border-gray-700 peer-checked:bg-gray-50 flex cursor-pointer select-none  border border-gray-300 p-4"
+                htmlFor="radio_2"
+              >
+                <img
+                  className="w-14 object-contain"
+                  src="/images/oG8xsl3xsOkwkMsrLGKM4.png"
+                  alt=""
+                />
+                <div className="ml-5">
+                  <span className="mt-2 font-semibold">DHL Delivery</span>
+                  <p className="text-slate-500 text-sm leading-6">
+                    Delivery: 2-4 Days
+                  </p>
+                </div>
+              </label>
+            </div>
+          </form>
+        </div>
+
+        <div className="mt-10 bg-gray-50 px-4 pt-8 lg:mt-2 mb-2">
+          <p className="text-xl font-medium">Payment Details</p>
+          <p className="text-gray-400">
+            Complete your order by providing your payment details.
+          </p>
+          <div>
+            <label
+              htmlFor="email"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Email
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="email"
+                name="email"
+                className="w-full  border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="your.email@gmail.com"
+              />
+              <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <label
+              htmlFor="card-holder"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Card Holder
+            </label>
+            <div className="relative">
+              <input
+                type="text"
+                id="card-holder"
+                name="card-holder"
+                className="w-full  border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="Your full name here"
+              />
+              <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-4 w-4 text-gray-400"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M4 7h16M4 11h16m-7 4h7"
+                  />
+                </svg>
+              </div>
+            </div>
+
+            <label
+              htmlFor="card-no"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Card Details
+            </label>
+            <div className="flex">
+              <div className="relative w-7/12 flex-shrink-0">
+                <input
+                  type="text"
+                  id="card-no"
+                  name="card-no"
+                  className="w-full  border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="xxxx-xxxx-xxxx-xxxx"
+                />
+                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-4 w-4 text-gray-400"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 7h16M4 11h16m-7 4h7"
+                    />
+                  </svg>
+                </div>
+              </div>
+              <input
+                type="text"
+                name="credit-expiry"
+                className="w-1/6 flex-shrink-0  border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="MM/YY"
+              />
+              <input
+                type="text"
+                name="credit-cvc"
+                className="w-1/6 flex-shrink-0  border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                placeholder="CVC"
+              />
+            </div>
+
+            <label
+              htmlFor="billing-address"
+              className="mt-4 mb-2 block text-sm font-medium"
+            >
+              Billing Address
+            </label>
+            <div className="flex flex-col sm:flex-row">
+              <div className="relative flex-shrink-0 sm:w-7/12">
+                <input
+                  type="text"
+                  id="billing-address"
+                  name="billing-address"
+                  className="w-full  border border-gray-200 px-4 py-3 pl-11 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500"
+                  placeholder="Street Address"
+                />
+                <div className="pointer-events-none absolute inset-y-0 left-0 inline-flex items-center px-3">
+                  <img
+                    className="h-4 w-4 object-contain"
+                    src="/images/6GOr6OXKHuCGhXrKEUv1u.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <select
+                name="billing-state"
+                className="mt-3 flex-shrink-0  border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500 sm:ml-3 sm:mt-0 sm:w-1/6"
+              >
+                <option value="State">State</option>
+              </select>
+              <input
+                type="text"
+                name="billing-zip"
+                className="mt-3 flex-shrink-0  border border-gray-200 px-4 py-3 text-sm shadow-sm outline-none focus:z-10 focus:border-blue-500 focus:ring-blue-500 sm:ml-3 sm:mt-0 sm:w-1/6"
+                placeholder="ZIP"
+              />
+            </div>
+
+            <div className="mt-6 border-t border-b py-2">
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Subtotal</p>
+                <p className="font-semibold text-gray-900">
+                  $
+                  {cartItems
+                    .reduce((acc, item) => acc + item.price, 0)
+                    .toFixed(2)}
+                </p>
+              </div>
+              <div className="flex items-center justify-between">
+                <p className="text-sm font-medium text-gray-900">Shipping</p>
+                <p className="font-semibold text-gray-900">₹8.00</p>
+              </div>
+            </div>
+            <div className="mt-6 flex items-center justify-between">
+              <p className="text-sm font-medium text-gray-900">Total</p>
+              <p className="text-2xl font-semibold text-gray-900">
+              ₹
+                {(
+                  cartItems.reduce((acc, item) => acc + item.price, 0) + 8
+                ).toFixed(2)}
+              </p>
+            </div>
+          </div>
+          <button className="mt-4 mb-8 w-full  bg-gray-900 px-6 py-3 font-medium text-white">
+            Place Order
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { createPopper } from "@popperjs/core";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../Redux/authSlice";
 import { IoHomeOutline } from "react-icons/io5";
+import { getCartItems } from "../Redux/cartSlice";
 
 const Navbar = ({ toggleModal }) => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -12,6 +13,7 @@ const Navbar = ({ toggleModal }) => {
   const buttonRef = useRef(null);
   const popoverRef = useRef(null);
   const { userInfo } = useSelector((state) => state.auth);
+  const { cart } = useSelector((state) => state.cart);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -20,7 +22,8 @@ const Navbar = ({ toggleModal }) => {
         placement: "bottom",
       });
     }
-  }, [isPopoverVisible]);
+    dispatch(getCartItems());
+  }, [isPopoverVisible, dispatch]);
 
   const handleButtonClick = () => {
     setIsPopoverVisible((prev) => !prev);
@@ -110,10 +113,20 @@ const Navbar = ({ toggleModal }) => {
                           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                         />
                       </svg>
-                      <span className="flex absolute -mt-5 ml-4">
-                        <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-                      </span>
+                      {cart.length > 0 ? (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="bg-red-500 text-white px-2 py-1 rounded-full text-[10px]">
+                            {cart.length}
+                          </span>
+                        </span>
+                      ) : (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                          <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                            {" "}
+                          </span>
+                        </span>
+                      )}
                     </Link>
                     {/* POPOVER */}
                     <div className="relative">
@@ -217,10 +230,20 @@ const Navbar = ({ toggleModal }) => {
                           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                         />
                       </svg>
-                      <span className="flex absolute -mt-5 ml-4">
-                        <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-                      </span>
+                      {cart.length > 0 ? (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="bg-red-500 text-white px-2 py-1 rounded-full text-[10px]">
+                            {cart.length}
+                          </span>
+                        </span>
+                      ) : (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                          <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                            {" "}
+                          </span>
+                        </span>
+                      )}
                     </Link>
 
                     <button
@@ -294,10 +317,20 @@ const Navbar = ({ toggleModal }) => {
                           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                         />
                       </svg>
-                      <span className="flex absolute -mt-5 ml-4">
-                        <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-                      </span>
+                      {cart.length > 0 ? (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="bg-red-500 text-white px-2 py-1 rounded-full text-[10px]">
+                            {cart.length}
+                          </span>
+                        </span>
+                      ) : (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                          <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                            {" "}
+                          </span>
+                        </span>
+                      )}
                     </Link>
                     <button
                       ref={buttonRef}
@@ -394,10 +427,20 @@ const Navbar = ({ toggleModal }) => {
                           d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                         />
                       </svg>
-                      <span className="flex absolute -mt-5 ml-4">
-                        <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                        <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500"></span>
-                      </span>
+                      {cart.length > 0 ? (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="bg-red-500 text-white px-2 py-1 rounded-full text-[10px]">
+                            {cart.length}
+                          </span>
+                        </span>
+                      ) : (
+                        <span className="flex absolute -mt-5 ml-4">
+                          <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                          <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                            {" "}
+                          </span>
+                        </span>
+                      )}
                     </Link>
                     {/* Sign In / Register */}
                     <button
